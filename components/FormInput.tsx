38a880808472b3c3880388fd00b57e5f8ff5aa5b
Gerald Fuchs <Geraldf@users.noreply.github.com>
2024-04-21 10:25:41 +0200

    working input

--- components/FormInput.tsx
+++ components/FormInput.tsx
@@ -1,28 +1,31 @@
-'use client'
+'use client';
 
-import { InputHTMLAttributes, forwardRef } from 'react'
-import { Input } from './ui/input'
-import { Label } from './ui/label'
+import { InputHTMLAttributes, forwardRef } from 'react';
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
-  error?: string
-  label: string
+  error?: string;
+  label: string;
 }
 
-function FormInput(props: Props) {
-  const { className, label, error, required, name } = props
+const Input = forwardRef<HTMLInputElement, Props>(function Input(
+  { name, error, label, required, className, ...rest },
+  ref
+) {
   return (
-    <fieldset className={`flex flex-col pt-3  ${className ? className : ''}`}>
-      <Label htmlFor={name} className='mb-1'>{label + (required ? ' *' : '')}</Label>
-
+    <fieldset className={`flex flex-col ${className ? className : ''}`}>
+      <label htmlFor={name} className="text-sm font-normal mb-1 pt-2 w-fit">
+        {label + (required ? ' *' : '')}
+      </label>
       <input
+        ref={ref}
         id={name}
         name={name}
-        className='w-full rounded-sm p-1 shadow-md ring-1 ring-gray-100'
+        {...rest}
+        className="ring-1 ring-gray-300 rounded-sm p-2 shadow-md w-full"
       />
-      {error && <span className='text-sm text-red-600'>{error}</span>}
+      {error && <span className="text-sm text-red-600">{error}</span>}
     </fieldset>
-  )
-}
+  );
+});
 
-export default FormInput
+export default Input;
